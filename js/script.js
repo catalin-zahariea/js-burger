d26b93d4ac9c4728db72c858b400a926ded457cc
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-03-30 16:41:01 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -15,7 +15,30 @@ btnPriceCalculate.addEventListener('click', function () {
     // Variables
 
     var totalPrice = 50;
-    
+    var ingredientItems = document.getElementsByClassName('ingredient-check');
+
+    console.log(ingredientItems);
+
+    // ________________________________
+    // Calculate totalPrice with a FOR Loop that checks whether or not additional extras
+    // were selected and adds their price to the totalPrice.
+
+    for ( var i = 0; i < ingredientItems.length; i++) {
+
+        // ________________________________
+        // Splitting ingredientItem |checked boolean| and |value| 
+        // into two(2) variables for ease of use.
+        var ingredientItemCheck = ingredientItems[i].checked;
+        var ingredientItemPrice = parseInt( ingredientItems[i].value );
+
+        // ________________________________
+        // If the item is check it will add it's price to the totalPrice
+        if ( ingredientItemCheck == true ) {
+
+            totalPrice += ingredientItemPrice;
+        }
+
+    } 
 
     // ________________________________
     // Print totalPrice
